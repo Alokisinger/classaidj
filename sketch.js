349338b02220a32629ea0615d8f7c5b504be4a40
Alokisinger <84913937+Alokisinger@users.noreply.github.com>
2022-05-31 20:06:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var peter_pan = "";
+function preload()
+{
+	peter_pan = loadSound("peter_pan.mp3");
+}
+
+function play()
+{
+     peter_pan.play();	
+}
+//Define stop_music() function which is called on click of "Stop Music" button and write code to stop the audio from playing inside this function (Hint: use p5 stop() function to stop the audio)
+
+
+
+
+
+function setup(){
+}
